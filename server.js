a0b5664ef076b611b4246d594a8fe279835cef9b
FullofMyself <wmaseko1@live.com>
2025-10-12 01:23:31 +0200

    CORS

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors({
     origin: [
         'http://localhost:5000',
         'http://127.0.0.1:3000',
+        'http://127.0.0.1:5000',
         'https://fullomyself.github.io'
     ],
     credentials: true,
